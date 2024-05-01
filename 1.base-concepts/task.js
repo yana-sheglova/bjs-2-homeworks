4d9c56bc826e7e612db967b3e3c86c43e25d4c42
Yana Sheglova <yana.shheglova.98@bk.ru>
2024-05-01 16:15:01 +0300

    Task1 done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+	let arr = [];
+	let d = b ** 2 - 4 * a * c;
+
+	if (d < 0) {
+		return arr;
+	} else if (d === 0) {
+		arr = [-b / (2 * a)];
+	} else if (d > 0) {
+		arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+	};
+	return arr;
+	console.log(arr);
+};
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+	let p = percent / 100 / 12;
+	let s = amount - contribution;
+	let paymet = s * (p + (p / (((1 + p) ** countMonths) - 1)));
+	let totalAmount = paymet * countMonths;
+	return +totalAmount.toFixed(2);
+}5
