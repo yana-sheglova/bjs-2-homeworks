9b22ac96bf18e7c8e7c457217feb947be7b84d2b
Yana Sheglova <yana.shheglova.98@bk.ru>
2024-05-02 20:20:58 +0300

    Task2 done

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -14,20 +14,56 @@ function getArrayParams(...arr) {
 };
 
 function summElementsWorker(...arr) {
-
-}
+	if (arr.length === 0) {
+		return 0;
+	};
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	};
+	return sum;
+};
 
 function differenceMaxMinWorker(...arr) {
-
-}
+	if (arr.length === 0) {
+		return 0;
+	};
+	let max = Math.max(...arr);
+	let min = Math.min(...arr);
+	let difference = max - min;
+	return difference;
+};
 
 function differenceEvenOddWorker(...arr) {
-
-}
+	if (arr.length === 0) {
+		return 0;
+	};
+	let sumEvenElement = 0;
+	let sumOddElement = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 === 0) {
+			sumEvenElement += arr[i];
+		} else {
+			sumOddElement += arr[i];
+		};
+	};
+	return sumEvenElement - sumOddElement;
+};
 
 function averageEvenElementsWorker(...arr) {
-
-}
+	if (arr.length === 0) {
+		return 0;
+	};
+	let sumEvenElement = 0;
+	let countEvenElement = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 === 0) {
+			sumEvenElement += arr[i];
+			countEvenElement++;
+		};
+	};
+	return sumEvenElement / countEvenElement;
+};
 
 function makeWork (arrOfArr, func) {
 
