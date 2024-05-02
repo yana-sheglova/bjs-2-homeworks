e4dcf45405a0aa2f1dd92282ed8301b6e585f835
Yana Sheglova <yana.shheglova.98@bk.ru>
2024-05-02 19:58:51 +0300

    Task1 done

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,17 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
-}
+	let min = Math.min(...arr);
+	let max = Math.max(...arr);
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+	let avg = +(sum / arr.length).toFixed(2);
+	return {
+		min: min,
+		max: max,
+		avg: avg
+	};
+};
 
 function summElementsWorker(...arr) {
 
