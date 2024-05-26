0e7b524761833d35c379bae6861a42320da108f2
Yana Sheglova <yana.shheglova.98@bk.ru>
2024-05-26 20:45:45 +0300

    Task1

---
+++ 4.objects/task.js
@@ -0,0 +1,34 @@
+function Student(name, gender, age, marks) {
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
+};
+
+let student1 = new Student("Yana", "female", 25);
+let student2 = new Student("Olesya", "female", 26);
+let student3 = new Student("Vlad", "male", 25);
+
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
+};
+
+Student.prototype.addMarks = function(...marks) {
+    if (this.hasOwnProperty('marks')) {
+        this.marks.push(...marks)
+      }
+};
+
+Student.prototype.getAverage = function() {
+	if (this.marks.length === 0) {
+		return 0;
+	} else {
+		return this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length;
+	}
+};
+
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
+};
